018595c7ae27730767aac2bd25fe26224d89d329
Saturn91 <manuel.geissberger@gmail.com>
2016-06-26 17:08:02 +0200

    [TUT3][WORK] Basic Rectangle in Front of red background

--- ThinMatrixTutorial/src/EnginTester/MainGameLoop.java
+++ ThinMatrixTutorial/src/EnginTester/MainGameLoop.java
@@ -3,6 +3,9 @@ package EnginTester;
 import org.lwjgl.opengl.Display;
 
 import renderEngine.DisplayManager;
+import renderEngine.Loader;
+import renderEngine.RawModel;
+import renderEngine.Renderer;
 
 public class MainGameLoop {
 
@@ -10,13 +13,33 @@ public class MainGameLoop {
 		
 		
 		DisplayManager.createDisplay();
+		
+		Loader loader = new Loader();
+		Renderer renderer = new Renderer();
+		
+		//Pseudo Data
+		float[] vertices = {
+			//Left bottom triangle
+			-0.5f, 0.5f, 0f,
+			-0.5f, -0.5f, 0f,
+			0.5f, -0.5f, 0f,
+			
+			//Right top triangle
+			0.5f, -0.5f, 0f,
+			0.5f, 0.5f, 0f,
+			-0.5f, 0.5f, 0f,
+		};
+		
+		RawModel model = loader.loadToVAO(vertices);
 
 		//actual Gameloop
 		while(!Display.isCloseRequested()){
-					
+			renderer.prepare();	
+			renderer.render(model);
 			DisplayManager.updateDisplay();
 		}
 		
+		loader.cleanUp();
 		DisplayManager.closeDisplay();
 	}
 
---
+++ ThinMatrixTutorial/src/renderEngine/Loader.java
@@ -0,0 +1,62 @@
+package renderEngine;
+
+import java.nio.FloatBuffer;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.lwjgl.BufferUtils;
+import org.lwjgl.opengl.GL11;
+import org.lwjgl.opengl.GL15;
+import org.lwjgl.opengl.GL20;
+import org.lwjgl.opengl.GL30;
+
+public class Loader {
+	
+	private List<Integer> vaos = new ArrayList<>();
+	private List<Integer> vbos = new ArrayList<>();
+	
+	public RawModel loadToVAO(float[] positions){
+		int vaoID = createVAO();
+		storeDataInAttributeList(0, positions);				//we save the data in list 0!
+		unbindVAO();
+		return new RawModel(vaoID, positions.length/3);		// /3 because each Point has 3 - coordinates
+	}
+	
+	public void cleanUp(){
+		for(int vao: vaos){
+			GL30.glDeleteVertexArrays(vao);
+		}
+		
+		for(int vao: vaos){
+			GL15.glDeleteBuffers(vao);
+		}
+	}
+	
+	private int createVAO(){
+		int voaID = GL30.glGenVertexArrays();
+		vaos.add(voaID);
+		GL30.glBindVertexArray(voaID);
+		return voaID;
+	}
+	
+	private void storeDataInAttributeList(int attributNumber, float[] data){
+		int vboID = GL15.glGenBuffers();
+		vbos.add(vboID);
+		GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, vboID);
+		FloatBuffer buffer = storeDataInFloatBuffer(data);
+		GL15.glBufferData(GL15.GL_ARRAY_BUFFER, buffer, GL15.GL_STATIC_DRAW);
+		GL20.glVertexAttribPointer(attributNumber, 3, GL11.GL_FLOAT, false, 0, 0);
+		GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, 0);	//unbind Buffer
+	}
+	
+	private void unbindVAO(){
+		GL30.glBindVertexArray(0); //unbind Vertex
+	}
+	
+	private FloatBuffer storeDataInFloatBuffer(float[] data){
+		FloatBuffer buffer = BufferUtils.createFloatBuffer(data.length);
+		buffer.put(data);
+		buffer.flip();
+		return buffer;
+	}
+}
