a7d7b4e1ec9940d7a916fa16dab153d0c7335540
Saturn91 <manuel.geissberger@gmail.com>
2016-06-29 18:45:32 +0200

    [TUT7][Step] doesen't work needs tut8 -> i belive Error in fragShader!

--- ThinMatrixTutorial/src/shaders/ShaderProgramm.java
+++ ThinMatrixTutorial/src/shaders/ShaderProgramm.java
@@ -2,15 +2,20 @@ package shaders;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.nio.FloatBuffer;
 
+import org.lwjgl.BufferUtils;
 import org.lwjgl.opengl.GL11;
 import org.lwjgl.opengl.GL20;
+import org.lwjgl.util.vector.Matrix4f;
+import org.lwjgl.util.vector.Vector3f;
 
 public abstract class ShaderProgramm {
 	
 	protected int programmID;
 	private int vertexShaderID;
 	private int fragmentShaderID;
+	private static FloatBuffer matrixBuffer = BufferUtils.createFloatBuffer(16);
 	
 	public ShaderProgramm(String vertexFile, String fragmentFile){
 		vertexShaderID = loadShader(vertexFile, GL20.GL_VERTEX_SHADER);
@@ -21,7 +26,14 @@ public abstract class ShaderProgramm {
 		bindAttributes();
 		GL20.glLinkProgram(programmID);
 		GL20.glValidateProgram(programmID);
+		getAllUniformLocations();
 		
+	}
+	
+	protected abstract void getAllUniformLocations();
+	
+	protected int getUniformLocation(String uniformName){
+		return GL20.glGetUniformLocation(programmID, uniformName);
 	}
 	
 	public void start(){
@@ -45,6 +57,28 @@ public abstract class ShaderProgramm {
 	
 	protected void bindAttribute(int atrribut, String variableName){
 		GL20.glBindAttribLocation(programmID, atrribut, variableName);
+	}
+	
+	protected void loadFloat(int location, float value){
+		GL20.glUniform1f(location, value);
+	}
+	
+	protected void loadVector(int location, Vector3f vector){
+		GL20.glUniform3f(location, vector.x, vector.y, vector.y);
+	}
+	
+	protected void loadBoolean(int location, boolean value){
+		float toLoad = 0;
+		if(value){
+			toLoad = 1;
+		}
+		GL20.glUniform1f(location, toLoad);
+	}
+	
+	protected void loadMatrix(int location, Matrix4f matrix){
+		matrix.store(matrixBuffer);
+		matrixBuffer.flip();
+		GL20.glUniformMatrix4(location, false, matrixBuffer);
 	}
 	
 	private static int loadShader(String file, int type){
--- ThinMatrixTutorial/src/shaders/StaticShader.java
+++ ThinMatrixTutorial/src/shaders/StaticShader.java
@@ -1,10 +1,13 @@
 package shaders;
 
+import org.lwjgl.util.vector.Matrix4f;
+
 public class StaticShader extends ShaderProgramm{
 	
 	private static final String VERTEX_FILE = "src/shaders/vertexShader.txt";
 	private static final String FRAGMENT_FILE = "src/shaders/fragmentShader.txt";
 	
+	private int location_transformationMatrix;
 
 	public StaticShader() {
 		super(VERTEX_FILE, FRAGMENT_FILE);
@@ -14,6 +17,15 @@ public class StaticShader extends ShaderProgramm{
 	protected void bindAttributes() {
 		super.bindAttribute(0, "position");			//connect position variable to 0-Attribute of VB0
 		super.bindAttribute(1, "textureCoords");	//connect texturecoords to 1-Attribute of VBO
+	}
+
+	@Override
+	protected void getAllUniformLocations() {
+		location_transformationMatrix = super.getUniformLocation("transformationMatrix");
+	}
+	
+	public void loadTransformationMatrix(Matrix4f matrix){
+		super.loadMatrix(location_transformationMatrix, matrix);
 	}
 
 }
